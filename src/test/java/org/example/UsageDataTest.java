e8f3254c140ad416bd6761cb7a52eb55e1db72df
RodrigoFayer <rodrigofayer26@gmail.com>
2024-07-30 19:56:16 -0300

    Creating iterator example

---
+++ src/test/java/org/example/UsageDataTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UsageDataTest {
+    @Test
+    void shouldCountNonArchivedProjects() {
+        Project project1 = new Project("Client 1", false);
+        Project project2 = new Project("Client 2", true);
+        Project project3 = new Project("Client 3", false);
+        Company company = new Company(project1, project2, project3);
+
+        assertEquals(2, UsageData.countNonArchivedProjects(company));
+    }
+
+
+    @Test
+    void shouldCountAllCompanyProjects() {
+        Project project1 = new Project("Client 1", false);
+        Project project2 = new Project("Client 2", true);
+        Project project3 = new Project("Client 3", false);
+        Company company = new Company(project1, project2, project3);
+
+        assertEquals(3, UsageData.countAllCompanyProjects(company));
+    }
+}
